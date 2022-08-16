b52f41b909e7fee2563fae568162e79a2b092213
Ian Abel <ian@utumno.org.uk>
2022-08-16 13:47:02 -0400

    more useful python scripts

--- TaylorSedovSolution.py
+++ TaylorSedovSolution.py
@@ -5,25 +5,16 @@ import numpy as np
 import sys
 
 Time = float(sys.argv[1])
-rMin = float(sys.argv[2])
-rMax = float(sys.argv[3])
-
-NCELLS = int(sys.argv[4])
-IE = float(sys.argv[5])
-
-dr = (rMax - rMin)/(NCELLS)
-
-answer = np.zeros( (NCELLS,4) )
+r    = float(sys.argv[2])
+IE   = float(sys.argv[3])
 
 tsProblem = TS.TaylorSedov( 1.0, IE, 5.0/3.0 )
 
 print("At t = %f, the shock front is at r = %.8f" % (Time,tsProblem.ShockR(Time)))
-print("# r\trho\tu\tp")
-for i in range(NCELLS):
-    r = rMin + (dr)*(i + 0.5)
-    answer[i,0] = r
-    answer[i,1:4] = tsProblem.Solution( Time, r )
-    print("%.8f\t%.8f\t%.8f\t%.8f" % tuple(answer[i,:]))
+
+print(" Rho = %.8f" % tsProblem.TaylorSedovRho(Time,r))
+print(" u_r = %.8f" % tsProblem.TaylorSedovU(Time,r))
+print(" P   = %.8f" % tsProblem.TaylorSedovP(Time,r))
 
 
 
